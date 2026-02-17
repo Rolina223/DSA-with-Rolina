692f0693725b558b47927fd95835905c746d9d04
Rolina223 <gopalireb223@gmail.com>
2026-02-17 10:33:57 +0530

    Add VarArgs example demonstrating Java varargs
    Add src/Function/VarArgs.java as a small demo of Java variable arguments. The new class includes a main that calls func(11, 22, ...), and a static func(int a, int b, String... v) which prints the two ints and the varargs contents using Arrays.toString.

---
+++ src/Function/VarArgs.java
@@ -0,0 +1,16 @@
+package Function;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+
+        func(11,22,"Rolina" ," Liza" ,"Krishna" ,"Bharti");
+    }
+    
+    // VarArgs -> (String..v -> Takes as much input)
+    static void func(int a, int b,String...v)
+    {
+        System.out.println(a + " " + b + " " + Arrays.toString(v));
+    }
+}
