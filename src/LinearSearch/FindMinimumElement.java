38a15f309f49bc4752aec07a1e67fa0cac01f030
Rolina223 <gopalireb223@gmail.com>
2026-02-24 15:19:52 +0530

    Add FindMinimumElement example
    Add a new Java class LinearSearch.FindMinimumElement that implements a simple linear scan to find the minimum element in an int array. The class includes a main method with a sample array and a searchMin method that returns -1 for an empty array and otherwise returns the minimum value.

---
+++ src/LinearSearch/FindMinimumElement.java
@@ -0,0 +1,30 @@
+package LinearSearch;
+
+public class FindMinimumElement {
+    public static void main(String[] args) {
+
+        int arr[] ={1,3,-21,12,-21,-21,-21,-21,23,24,19,-21,90,-109,66};
+        System.out.println(searchMin(arr));
+
+    }
+
+    static int searchMin(int arr[])
+    {
+        if(arr.length==0)
+        {
+            return -1;
+        }
+
+        int i =0;
+        int min = arr[i];
+
+        for (int j = 0; j < arr.length; j++) {
+            if(arr[j]<min)
+            {
+                min =arr[j];
+            }
+        }
+        return min;
+    }
+
+}
